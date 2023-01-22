1aa8455a99bd8a5cf6aff8a931d43933ba97795d
abeer117 <nabeer161117@bscse.uiu.ac.bd>
2023-01-23 05:13:15 +0600

    index.js file for backend info using mongodb

---
+++ seeds/index.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const cities = require('./cities')
+const { places,descriptors } = require('./seedHelpers')
+const Campground = require('../models/campgrounds')
+
+
+
+////////mongoose connection//////////
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp',{useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>{
+        console.log('Mongoose connection open!')
+    })
+    .catch(err=>{
+        console.log('Error Not working')
+        console.log(err)
+    })
+
+////////mongoose connection//////////
+
+
+///array for random numbers////
+const sample = array => array[Math.floor(Math.random() * array.length)]
+
+/////////////////////////
+
+
+const seedDB = async() => {
+    await Campground.deleteMany({})
+    for(let i = 0; i<50; i++){
+        const random1000 = Math.floor(Math.random()*1000)
+        const price = Math.floor(Math.random()*20) + 10
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nostrum tempore id tenetur',
+            price
+        })
+        await camp.save()
+
+    }
+
+}
+seedDB().then(()=>{
+    mongoose.connection.close()
+
+})
